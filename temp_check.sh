4147bd2b2ebd16bea17e0f1507e0055502a5e1d5
bharathappali <bharath.appali@gmail.com>
2023-01-13 18:53:01 +0530

    adding temp files
    Signed-off-by: bharathappali <bharath.appali@gmail.com>

---
+++ temp_check.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo -n "Deleting minikube ... "
+minikube delete
+echo "Done."
+
+echo "Starting minikube"
+minikube start
+
+mkdir -p /tmp/pronosana
+
+git clone https://github.com/bharathappali/pronosana
+cd pronosana
+kubectl apply -f test
+sleep 120
+deployment_name=$(kubectl get pods | grep "pronosana-deployment" | cut -d " " -f1)
+kubectl cp ${PWD}/tmp_data/omf_data.txt default/${deployment_name}:/home -c prometheus
+kubectl exec -it ${deployment_name} -c prometheus promtool tsdb create-blocks-from openmetrics /home/omf_data.txt /prometheus
+
+IP=$(minikube ip)
+firefox -new-tab "http://${IP}:30300"
